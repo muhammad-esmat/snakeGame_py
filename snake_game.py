f95ab80a37258232524393cc592b088ffadfde6a
Muhammad Esmat <muhammadesmat.develops@gmail.com>
2026-05-02 19:25:48 +0300

    Initial commit

---
+++ snake_game.py
@@ -0,0 +1,147 @@
+#import libraries
+import sys
+print(sys.version)
+import curses as curses
+import random
+import time
+
+#If snake loses
+def loser(mid, snake, score):
+   print("Snake hit!")
+   print(f"snake[0] = {snake[0]}, snake[1:] = {snake[1:]} ")
+   curses.beep()
+   window.clear()
+   window.addstr(mid[0], mid[1], score)
+   window.refresh()
+   time.sleep(10)
+   curses.endwin()
+   quit()
+   #Add more features
+
+#init curses and screen
+screen = curses.initscr()
+
+#getmaxyx
+scr_y, scr_x = screen.getmaxyx()
+
+#hide the mouse cursur
+curses.curs_set(0)
+
+#init window
+window = curses.newwin(scr_y, scr_x, 0, 0)
+
+#allow input
+window.keypad(1)
+
+#set delay time
+window.timeout(125)
+
+#make snake head
+snk_y = scr_y // 2
+snk_x = scr_x // 4
+
+#make snake list
+snake = [
+   [snk_y, snk_x],
+   [snk_y, snk_x - 1],
+   [snk_y, snk_x - 2]
+]
+
+#Get snake direction
+def snk_dirc(snk1, snk2): #Works perfectly in all directions except when moving left
+   dirc = "H"
+   if snk1[0] != snk2[0]:
+      dirc = "V"
+   
+   return dirc
+
+
+#make middle of screen
+mid = [scr_y // 2, scr_x // 2]
+
+#make food list
+food = [mid[0], mid[1]]
+
+#make food
+window.addch(food[0], food[1], curses.ACS_STERLING)
+
+#Set the keys
+KEY_RIGHT = 454
+KEY_LEFT = 452
+KEY_UP = 450
+KEY_DOWN = 456
+
+#set initial key to right
+key = KEY_RIGHT
+
+#Score
+score = f"Score: {len(snake)}"
+
+#start the game loop
+while True:
+   print(f"snake: {snake}")
+   print(snk_dirc(snake[0], snake[1]))
+
+   #get next key
+   next_key = window.getch()
+   print(f"next_key = {next_key}")
+   print(f'"KEY_RIGHT = 454", "KEY_LEFT = 452", "KEY_UP = 450", "KEY_DOWN = 456"')
+   
+   #if no key is entered or snake moves in the same direction key stays right
+   if next_key == -1 or snk_dirc(snake[0], snake[1]) == "V" and next_key in [KEY_UP, KEY_DOWN] or snk_dirc(snake[0], snake[1]) == "H" and next_key in [KEY_RIGHT, KEY_LEFT]:
+      pass
+   else:
+      key = next_key
+
+   print(f"key = {key}, next_key = {next_key}")
+
+   #check if snake hit itself or walls
+   if snake[0][0] in [1, scr_y-1] or snake[0][1] in [1, scr_x-1] or snake[0] in snake[1:]:
+      loser(mid, snake, score)
+   else:
+      #make new head based on direction and insert it into snake
+      new_head = [snake[0][0], snake[0][1]]
+      print(f"new_head(b4): {new_head}")
+      if key == 454:
+         print("KEY_RIGHT")
+         new_head[1] += 1
+      elif key == 452:
+         print("KEY_LEFT")
+         new_head[1] -= 1
+      elif key == 450:
+         print("KEY_UP")
+         new_head[0] -= 1
+      elif key == 456:
+         print("KEY_DOWN")
+         new_head[0] += 1
+      else:
+         print("KEY_RIGHT")
+         new_head[1] += 1
+
+      print(f"new_head(after)= {new_head}")
+      snake.insert(0, new_head)
+      
+   #check if snake ate food if so make it respawn somewhere else
+   if food == snake[0]:
+      food = None
+      while food is None:
+         new_food = [
+            random.randint(2,scr_y - 2), 
+            random.randint(2,scr_x - 2)
+            ]
+         food = new_food if new_food not in snake else None
+
+      window.addch(food[0], food[1], curses.ACS_STERLING)
+
+   #else remove tail 
+   else:
+      tail = snake.pop()
+      window.addch(tail[0], tail[1], ' ')
+    
+   #make snake
+   window.addch(snake[0][0], snake[0][1], curses.ACS_DIAMOND)
+
+   print("_______________________________________")
+
+   print(curses.__name__)
+
