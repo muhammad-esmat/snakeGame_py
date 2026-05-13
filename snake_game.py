cd65a53a6b3bacddc9ec3396ec86c549830f8a87
Muhammad Esmat <muhammadesmat.develops@gmail.com>
2026-05-13 16:00:49 +0300

    Better lose screen + less comments in main file

--- snake_game.py
+++ snake_game.py
@@ -1,89 +1,70 @@
-#import libraries
 import curses as curses
 import random
 from snakes import snake
 
-#init curses and screen
 screen = curses.initscr()
 
-#getmaxyx
 scr_y, scr_x = screen.getmaxyx()
 
-#initialize window
 window = curses.newwin(scr_y, scr_x, 0, 0)
 
-#hide the mouse cursur
 curses.curs_set(0)
 
-#allow input
 window.keypad(1)
 
-#set delay time
 window.timeout(100)
 
-#make list representing the middle of screen
 mid = [scr_y // 2, scr_x // 2]
 
-#initailize food coordinates list
 food = (mid[0], mid[1])
 
-#add food to screen
 window.addch(food[0], food[1], curses.ACS_STERLING)
 
-#make the snake object
 player = snake([scr_y // 2, scr_x // 4], curses.ACS_BOARD)
 
-#set initial key to right
 key = curses.KEY_RIGHT
 
-#start the game loop
 while True:
-   #get next key
-   next_key = window.getch()
-   
-   #if no key is entered or snake moves in the same direction key stays right
-   if (
-       next_key == -1
-       or player.line_of_motion() == "Vertical" and next_key in [curses.KEY_UP, curses.KEY_DOWN]
-       or player.line_of_motion() == "Horizontal" and next_key in [curses.KEY_RIGHT, curses.KEY_LEFT]
-   ):
-      pass
-   else:
-      key = next_key
-      
-      if key == curses.KEY_UP:
-          player.direction = "up"
-      elif key == curses.KEY_DOWN:
-          player.direction = "down"
-      elif key == curses.KEY_RIGHT:
-          player.direction = "right"
-      elif key == curses.KEY_LEFT:
-          player.direction = "left"
+    next_key = window.getch()
 
-   #make new head based on direction and insert it into snake
-   player.addHead()
- 
-   #check if snake ate food if so make it respawn somewhere else
-   if player.head == food:
-      food = None
-      while food is None:
-         new_food = (
-            random.randint(2,scr_y - 2),
-            random.randint(2,scr_x - 2)
-         )
-         food = new_food if not player.collides_with(food) else None
-      window.addch(food[0], food[1], curses.ACS_STERLING)
-   else:
+    if (
+        next_key == -1
+        or player.line_of_motion() == "Vertical"
+        and next_key in [curses.KEY_UP, curses.KEY_DOWN]
+        or player.line_of_motion() == "Horizontal"
+        and next_key in [curses.KEY_RIGHT, curses.KEY_LEFT]
+    ):
+        pass
+    else:
+        key = next_key
+
+        if key == curses.KEY_UP:
+            player.direction = "up"
+        elif key == curses.KEY_DOWN:
+            player.direction = "down"
+        elif key == curses.KEY_RIGHT:
+            player.direction = "right"
+        elif key == curses.KEY_LEFT:
+            player.direction = "left"
+
+    player.addHead()
+
+    if player.head == food:
+        food = None
+        while food is None:
+            new_food = (random.randint(2, scr_y - 2), random.randint(2, scr_x - 2))
+            food = new_food if not player.collides_with(food) else None
+        window.addch(food[0], food[1], curses.ACS_STERLING)
+    else:
         tail = player.popTail()
-        window.addch(tail[0], tail[1], ' ')
-   
-   if (
-        player.head[0] in [1, scr_y-1] or
-        player.head[1] in [1, scr_x-1] or
-        player.head in player.snakeBody[:-1]
-   ):
-      player.loser(mid, window)
-      quit()
-    
-   window.addch(player.head[0], player.head[1], player.skin)
+        window.addch(tail[0], tail[1], " ")
 
+    if (
+        player.head[0] in [1, scr_y - 1]
+        or player.head[1] in [1, scr_x - 1]
+        or player.head in player.snakeBody[:-1]
+    ):
+        player.loser(mid, window, screen)
+        quit()
+
+    window.addch(player.head[0], player.head[1], player.skin)
--- snakes.py
+++ snakes.py
@@ -1,13 +1,16 @@
 import curses as curses
+import time
+from ashiart import image_to_ascii
+
 
 class snake:
-    def __init__(self, coordinates, skin, direction = "right"):
+    def __init__(self, coordinates, skin, direction="right"):
         initBody = [
-            (coordinates[0], coordinates[1]-2),
-            (coordinates[0], coordinates[1]-1),
-            (coordinates[0], coordinates[1])
+            (coordinates[0], coordinates[1] - 2),
+            (coordinates[0], coordinates[1] - 1),
+            (coordinates[0], coordinates[1]),
         ]
-        
+
         self.snakeBody = initBody
 
         self.direction = direction
@@ -17,7 +20,6 @@ class snake:
         self.head = self.snakeBody[-1]
         self.tail = self.snakeBody[0]
 
-    
     def line_of_motion(self):
         LOM = "Horizontal"
 
@@ -32,20 +34,19 @@ class snake:
         else:
             return False
 
-    def addHead(self, steps = 1):
+    def addHead(self, steps=1):
         snake_head = self.head
 
         if self.direction == "right":
-            self.snakeBody.append((snake_head[0], snake_head[1]+steps))
+            self.snakeBody.append((snake_head[0], snake_head[1] + steps))
         elif self.direction == "left":
-            self.snakeBody.append((snake_head[0], snake_head[1]-steps))
+            self.snakeBody.append((snake_head[0], snake_head[1] - steps))
         elif self.direction == "up":
-            self.snakeBody.append((snake_head[0]-steps, snake_head[1]))
-        elif self.direction  == "down":
-            self.snakeBody.append((snake_head[0]+steps, snake_head[1]))
+            self.snakeBody.append((snake_head[0] - steps, snake_head[1]))
+        elif self.direction == "down":
+            self.snakeBody.append((snake_head[0] + steps, snake_head[1]))
 
         self.head = self.snakeBody[-1]
-
 
     def popTail(self):
         tail = self.snakeBody.pop(0)
@@ -55,13 +56,40 @@ class snake:
     def get_score(self):
         return str(len(self.snakeBody) - 3)
 
-    def loser(self, mid, window):
-        score = self.get_score()
+    def loser(self, mid, window, screen):
+        scr_y, scr_x = screen.getmaxyx()
 
-        window.clear()
+        lose_msg1 = "Snake hit!!"
+        lose_msg2 = f"Current score: {self.get_score()}"
+        lose_msg3 = "Press any key to exit ..."
+
+        curses.flushinp()
+        window.erase()
+
+        window.addstr(
+            mid[0] - 2, abs(mid[1] - (len(lose_msg1) // 2)), lose_msg1, curses.A_BOLD
+        )
+
+        window.addstr(
+            mid[0] + 2,
+            mid[1] - (len(lose_msg2) // 2),
+            lose_msg2,
+        )
+
         window.timeout(-1)
-
-        window.addstr(mid[0], mid[1], f"Score: {score}")
-
         window.getch()
+        time.sleep(1)
 
+        curses.flushinp()
+        window.erase()
+
+        window.addstr(
+            mid[0],
+            mid[1] - (len(lose_msg3) // 2),
+            lose_msg3,
+            curses.A_BLINK,
+        )
+
+        window.timeout(-1)
+        window.getch()
+        time.sleep(2)
